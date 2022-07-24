583e76e9822cfbd3445bfb8a594543621a7ae372
levantu.nd1997 <levantu.nd1997@gmail.com>
2022-07-24 21:53:14 +0700

    format code với prettier

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom'
 import Homepage from './pages/Home'
 import Newspage from './pages/News'
 import Contactpage from './pages/Contact'
 
-
 function App() {
-
-  return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/news">News</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Homepage/>} />
-        <Route path="/news" element={<Newspage/>} />
-        <Route path="/contact" element={<Contactpage/>} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/news" element={<Newspage />} />
+                <Route path="/contact" element={<Contactpage />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
